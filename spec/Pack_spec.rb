9e403827961c1ba1820ac27dbe929a1cbbcb1bc5
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-19 12:38:21 +1000

    Added new tests for Pack and WatermelonOrder

--- spec/Pack_spec.rb
+++ spec/Pack_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/Pack.rb'
 describe Pack do
     describe '#get quantity in pack' do
         it 'returns 3' do
-            pack = Pack.new(3)
+            pack = Pack.new(3, 6.99)
             expect(pack.qty).to eql(3)
         end
     end
@@ -13,8 +13,17 @@ end
 describe Pack do
     describe '#get price of pack' do
         it 'returns 6.99' do
-            pack = Pack.new(6.99)
-            expect(pack.qty).to eql(6.99)
+            pack = Pack.new(3, 6.99)
+            expect(pack.price).to eql(6.99)
+        end
+    end
+end
+
+describe Pack do
+    describe '#get value of pack' do
+        it 'returns 3/6.99' do
+            pack = Pack.new(3, 6.99)
+            expect(pack.value).to eql(3/6.99)
         end
     end
 end
--- spec/WatermelonOrder_spec.rb
+++ spec/WatermelonOrder_spec.rb
@@ -4,17 +4,28 @@ require_relative '../lib/WatermelonOrder.rb'
 describe WatermelonOrder do
     describe '#get quantity' do
         it 'returns 10' do
-            food = FoodOrder.new(10)
-            expect(food.qty).to eql(10)
+            watermelonOrder = WatermelonOrder.new(10)
+            expect(watermelonOrder.qty).to eql(10)
         end
     end
 end
 
 describe WatermelonOrder do
-    describe '#get pack 0' do
+    describe '#get attributes of first pack' do
         it 'returns Pack.new(3, 6.99)' do
-            watermelons = WatermelonOrder.new(100)
-            expect(pack[0]).to eql(Pack.new(3,6.99))
+            watermelonOrder = WatermelonOrder.new(100)
+            expect(watermelonOrder.packs[0].qty).to eql(3)
+            expect(watermelonOrder.packs[0].price).to eql(6.99)
+            expect(watermelonOrder.packs[0].value).to eql(3/6.99)
+        end
+    end
+end
+
+describe WatermelonOrder do
+    describe '#get number of packs offered' do
+        it 'returns 3' do
+            watermelonOrder = WatermelonOrder.new(1)
+            expect(watermelonOrder.packs.length).to eql(2)            
         end
     end
 end
