7e5009cdbc27f9ca515fa7309566748c0cbab554
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-19 12:45:13 +1000

    Added pineapple code to satisfy tests

--- lib/PineappleOrder.rb
+++ lib/PineappleOrder.rb
@@ -1,2 +1,12 @@
-class PineappleOrder
+require_relative "FoodOrder"
+
+class PineappleOrder < FoodOrder
+    
+    def initialize(qty)
+        super(qty)
+        @packs << Pack.new(2, 9.95)
+        @packs << Pack.new(5, 16.95)
+        @packs << Pack.new(8, 16.95)
+        
+    end
 end
