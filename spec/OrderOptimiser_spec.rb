f58c342d945da64e7d7bcfcd4199aee9ef3f0f71
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-20 15:13:48 +1000

    Added test to OrderOptimiser for a different order, need to update optimise(x) to pass.

--- spec/OrderOptimiser_spec.rb
+++ spec/OrderOptimiser_spec.rb
@@ -10,15 +10,20 @@ RSpec.describe OrderOptimiser do
         # end
     
 
-        it 'returns a hash with one key("Watermelon"), and one value (hash with one key (Pack object with qty 3 and price 6.99) and one value (1)' do
+        it 'returns 1 x 3 pack @ $6.99 ea' do
             optimisedOrder = OrderOptimiser.optimise({"Watermelon" => 3})
             watermelon_packs = optimisedOrder["Watermelon"]
             expect(watermelon_packs.keys[0].qty).to eql(3)
             expect(watermelon_packs.keys[0].price).to eql(6.99)
             expect(watermelon_packs[watermelon_packs.keys[0]]).to eql(1)
-            # foodOrder = FoodOrder.new("Watermelon", 10)
-            # expect(foodOrder.name).to eql("Watermelon")
-            # expect(foodOrder.qty).to eql(10)
+        end
+
+        it 'returns 2 x 5 pack @ $8.99 ea' do
+            optimisedOrder = OrderOptimiser.optimise({"Watermelon" => 10})
+            watermelon_packs = optimisedOrder["Watermelon"]
+            expect(watermelon_packs.keys[0].qty).to eql(5)
+            expect(watermelon_packs.keys[0].price).to eql(8.99)
+            expect(watermelon_packs[watermelon_packs.keys[0]]).to eql(2)
         end
 
 
