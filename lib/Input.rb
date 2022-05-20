19afe46fae47c234c3b3f97c96cea00e9de09567
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-20 18:53:23 +1000

    Input.rb - changed items from class to local variable

--- lib/Input.rb
+++ lib/Input.rb
@@ -1,53 +1,54 @@
 require_relative 'AvailableFood.rb'
+require_relative 'OrderOptimiser.rb'
+require_relative 'Output.rb'
 
 class Input
-    @items = {}
     
-    def self.items
-        @items
-    end
-
     def self.displayMenu
+        items = {}
+
         puts "Hello, welcome to Fresho!"
 
         while true do
         
-        name = getName
+        name = getName(items)
         next if name == "Invalid"
         break if name == "Done"
         
-        qty = getQty(name)    
+        qty = getQty(items, name)    
         next if qty == "Back"
  
-        @items[name] = qty.to_i
-        puts "You ordered #{@items[name]} #{name}s!"
+        items[name] = qty.to_i
+        puts "You ordered #{items[name]} #{name}s!"
         end
 
-        if @items.length < 1
+        if items.length < 1
             puts "No items ordered. Thank you for browsing today's selection."
         else
-            puts "TODO"
-            puts items.keys
+            invoice = OrderOptimiser.optimise(items)
+            Output.display(invoice)
             #TODO method that outputs output from knapsack method.
         end
     end
 
-    def self.getName
+    
+
+    def self.getName(items)
         puts "Please enter the name of the food you would like to purchase. Today's available food: #{AvailableFood.packs.keys}. Alternatively, type 'Done' if you are ready to recieve your invoice."
         name = gets.chomp.strip.capitalize
         return name if name == "Done"
 
         #CHECK IF ITEM DOES NOT EXIST IN AVAILABLEFOOD
         if !AvailableFood.packs.keys.include? name
-            puts "#{name} "#is not available. Available items: #{AvailableFood.packs.keys}"
+            puts "#{name} is not available. Available items: #{AvailableFood.packs.keys}"
             return "Invalid"
         end
         #CHECK IF ITEM EXISTS IN ITEMS
-        puts "#{name} has already been ordered. Continuing will change the quanitity previously ordered." if @items.keys.include? name
+        puts "#{name} has already been ordered. Continuing will change the quanitity previously ordered." if items.keys.include? name
         return name
     end
 
-    def self.getQty(name)
+    def self.getQty(items, name)
         while true do
             puts "How many #{name}s would you like? Alternatively, type 'Back' to go back."
             qty = gets.chomp.capitalize
