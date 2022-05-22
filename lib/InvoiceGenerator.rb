dd14552f4cb047c733fb304a42cb6d93074356d0
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-22 19:21:31 +1000

    InvoiceGenerator: added comment explaining how to simulate example order. reversed commenting of code in order to recieve real user input.

--- lib/InvoiceGenerator.rb
+++ lib/InvoiceGenerator.rb
@@ -2,7 +2,8 @@ require_relative "Input.rb"
 require_relative "OrderOptimiser.rb"
 require_relative "Output.rb"
 
-# itemsOrdered = Input.displayMenu 
-# finalisedOrder = OrderOptimiser.optimise(itemsOrdered)
-finalisedOrder = OrderOptimiser.optimise({"Watermelon" => 10, "Pineapple" => 14, "Rockmelon" => 13})
+itemsOrdered = Input.displayMenu 
+finalisedOrder = OrderOptimiser.optimise(itemsOrdered)
+#uncomment above two lines, and un-uncomment below one line to simulate example order
+# finalisedOrder = OrderOptimiser.optimise({"Watermelon" => 10, "Pineapple" => 14, "Rockmelon" => 13})
 Output.display(finalisedOrder)
