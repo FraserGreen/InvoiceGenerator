cf11ebf70f4b87c18a39cd995f93bedaf334a47d
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-22 19:27:37 +1000

    OrderOptimiser_spec: removed old optimise() tests, added new optimise() tests, getMinCostOrder() tests, and sumSubset() tests. Also includes some commented out knapsack tests for the future when switching to knapsack algorithm.

--- spec/OrderOptimiser_spec.rb
+++ spec/OrderOptimiser_spec.rb
@@ -2,53 +2,63 @@ require 'rspec'
 require_relative '../lib/OrderOptimiser.rb'
 
 RSpec.describe OrderOptimiser do
-    describe '#optimise' do   
-
-        it 'items = {"Watermelon" => 10} returns 2 x 5 pack @ $8.99 ea' do
-            finalisedOrder = OrderOptimiser.optimise({"Watermelon" => 10})
-            food = finalisedOrder[finalisedOrder.keys[0]]
-            numPacks = food[food.keys[0]]
-            numFoodItemsInPack = food.keys[0].qty
-            packPrice = food.keys[0].price
-            expect(numPacks).to eql(2)
-            expect(numFoodItemsInPack).to eql(5)
-            expect(packPrice).to eql(8.99)
+    
+    describe '#sumSubset' do
+        packs = AvailableFood.packs["Watermelon"]
+        weights = []
+        values = []
+        
+        packs.each {|pack|
+            weights << pack.qty
+            values << pack.price
+        }
+        it 'returns correct indices of packs to include in order' do    
+            expect(OrderOptimiser.sumSubset(weights,10,values, [])).to eql([[1,1]]) #returns only two instances of packs[1]
+            expect(OrderOptimiser.sumSubset(weights,20,values, [])).to include([1,1,1,1]) #returns an array that includes two instances of packs[1]
+            expect(OrderOptimiser.sumSubset(weights,7,values, [])).to eql([]) #returns an empty array, meaning an exact match could not be made
+            
         end
-
-        # it 'returns values given in example invoice' do
-        #     finalisedOrder = OrderOptimiser.optimise({"Watermelon" => 10, "Pineapple" => 14, "Rockmelon" => 13})
-        #     puts finalisedOrder
-        #     food1 = finalisedOrder[finalisedOrder.keys[0]]
-        #     food2 = finalisedOrder[finalisedOrder.keys[1]]
-        #     food3 = finalisedOrder[finalisedOrder.keys[2]]
-        #     expect(food1[food1.keys[0]]).to eql(2)
-        #     expect(food1.keys[0].qty).to eql(5)
-        #     expect(food1.keys[0].price).to eql(8.99)
-
-        #     expect(food2[food2.keys[0]]).to eql(1)
-        #     expect(food2.keys[0].qty).to eql(8)
-        #     expect(food2.keys[0].price).to eql(24.95)
-        #     expect(food2[food2.keys[1]]).to eql(3)
-        #     expect(food2.keys[1].qty).to eql(2)
-        #     expect(food2.keys[1].price).to eql(9.95)
-
-        #     expect(food3[food3.keys[0]]).to eql(2)
-        #     expect(food3.keys[0].qty).to eql(5)
-        #     expect(food3.keys[0].price).to eql(9.95)
-        #     expect(food3[food3.keys[1]]).to eql(1)
-        #     expect(food3.keys[1].qty).to eql(3)
-        #     expect(food3.keys[1].price).to eql(5.95)
-        # end
-
-        # it 'total item qty is reached exactly by the packs' do
-        #     finalisedOrder = OrderOptimiser.optimise({"Watermelon" => 7})
-        #     finalisedOrder.each{ |order, packs|
-        #         totalQtyOfPacks = 0
-        #         packs.each { |pack, qty|
-        #             totalQtyOfPacks += pack.qty * qty
-        #         }
-        #         expect(order[1]).to eql(totalQtyOfPacks)
-        #     }
-        # end
     end
+
+    describe '#getMinCostOrder' do
+        packs = AvailableFood.packs["Watermelon"]
+        it 'returns {<Pack: qty:5, price: 8.99> => 2} when faced with an order of 10 watermelons' do
+            expect(OrderOptimiser.getMinCostOrder(packs, 10)).to eql({packs[1] => 2})
+        end
+        
+        it 'returns {<Pack: qty:5, price: 8.99> => 3, <Pack: qty:3, price: 6.99> => 1} when faced with an order of 18 watermelons' do
+            expect(OrderOptimiser.getMinCostOrder(packs, 18)).to eql({packs[1] => 3, packs[0] => 1})
+        end
+        
+        it 'returns {<Pack: qty:2, price: 9.95> => 2, <Pack: qty:5, price: 16.95> => 2} when faced with an order of 14 pineapples' do
+            packs = AvailableFood.packs["Pineapple"]
+            expect(OrderOptimiser.getMinCostOrder(packs, 14)).to eql({packs[0] => 2, packs[1] => 2})
+        end
+        
+    end
+    
+    describe '#optimise' do
+        it 'returns correct output for 10 watermelons' do
+            packs = AvailableFood.packs["Watermelon"]
+            orders = OrderOptimiser.optimise({"Watermelon" => 10})
+            expect(orders).to eql({["Watermelon", 10] => {packs[1] => 2}})
+        end
+        it 'returns correct output for example order' do
+            orders = OrderOptimiser.optimise({"Watermelon" => 10, "Pineapple" => 14, "Rockmelon" => 13})
+            expect(orders).to eql({
+                ["Watermelon", 10] => {AvailableFood.packs["Watermelon"][1] => 2},
+                ["Pineapple", 14] =>  {AvailableFood.packs["Pineapple"][0] => 2, 
+                                       AvailableFood.packs["Pineapple"][1] => 2},
+                ["Rockmelon", 13] => {AvailableFood.packs["Rockmelon"][0] => 1,
+                                       AvailableFood.packs["Rockmelon"][1] => 2}})
+        end
+    end
+
+    # describe '#knapsack' do
+    #     it 'returns correct total value with given weights and values' do
+    #         # expect(OrderOptimiser.knapsack([3, 5], [6.99, 8.99], 10)).to eql(2*(8.99))
+    #         # expect(OrderOptimiser.knapsack([2, 5, 8], [9.95, 16.95, 24.95], 13)).to eql((3*(9.95)+(24.95)))
+    #         # expect(OrderOptimiser.knapsack([3, 5, 9], [5.95, 9.95, 16.99], 13)).to eql((2*(9.95)+(5.95)))
+    #     end
+    
 end
