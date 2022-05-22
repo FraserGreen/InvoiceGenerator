ae51b929fe1d8db10abf2e2e769f679a85538ed7
FraserGreen <s3787186@student.rmit.edu.au>
2022-05-22 19:53:36 +1000

    InvoiceGenerator: Stopped displaying invoice if no items ordered. Added file description.

--- lib/InvoiceGenerator.rb
+++ lib/InvoiceGenerator.rb
@@ -2,8 +2,9 @@ require_relative "Input.rb"
 require_relative "OrderOptimiser.rb"
 require_relative "Output.rb"
 
+#driver file to generate invoice
 itemsOrdered = Input.displayMenu 
 finalisedOrder = OrderOptimiser.optimise(itemsOrdered)
 #uncomment above two lines, and un-uncomment below one line to simulate example order
 # finalisedOrder = OrderOptimiser.optimise({"Watermelon" => 10, "Pineapple" => 14, "Rockmelon" => 13})
-Output.display(finalisedOrder)
+Output.display(finalisedOrder) if finalisedOrder.length != 0
